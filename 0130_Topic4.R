9f19b8b656a272a5e2c4ef0957c914aba5c4f5a1
Yanshi Luo <kdrobian@hotmail.com>
2017-02-06 01:15:10 -0600

    With leaflet successfully run!!

--- 0130_Topic4.R
+++ 0130_Topic4.R
@@ -59,47 +59,57 @@ ggplot(d, aes(x=STATE_CODE_001)) +
        x="State Code", y = "Number of Bridges")
 
 
+# I searched in the google map to clear the LAT and LONG format
+# I tried some method to deal with them and I find out that (31061110 87341340) means (31.061110, -87341340)
+# So I tidied the data as follows
 
+library(stringr)
+D1$code <- str_pad(D1$code, 5, pad = "0")
+D1%>% group_by(code) %>% dplyr::summarise(count = n())
+
+
+D1 %>% mutate(LAT = LAT_016/1000000, LONG = LONG_017/-1000000) %>%
+  select(STATE_CODE_001, code, LAT, LONG)-> d2
+
+
+blingIcon <- makeIcon(
+  iconUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Emoji_u1f4b0.svg/1024px-Emoji_u1f4b0.svg.png",
+  iconWidth = 30, iconHeight = 30)
+
+
+#############################################################################
+# PLEASE DO NOT RUN THE FOLLOWING CODE ON YOUR COMPUTER UNLESS YOU WOULD LIKE TO
+# SPEND 20 MINUTED TO WAIT FOR ITS LOADING!!!!
+# IT IS VERY EASY TO CRASH!!!! SINCE THERE ARE TOO MAY DATA!!!
+# I ran it on my own Ubuntu Rstudio sever to do my own job when the map is drawing.
 ###########################################################################
-# I tried to do a data visualization with leaflet but my Rstudio server crashed 
-#   because of the format of the Longitude and Latitude.
-# It makes me not only fail to hand out in time but also did not make a cool visulization. T_T
-# A sad story.........
-# A sample leaflet webpage that I tried to follow up: 
+# This is a leaflet interactive map.
+# The idea comes from:
 # https://www.kaggle.com/taddallas/d/kaggle/college-scorecard/interactive-map
+#
+# I tried a lot of time to modify the data but it keeps crashing until I 
+#     finally figure out the lantitude and lontitude data format.
+# In fact, it crashes so many time that I failed to hand in my final work beore 12 pm T_T
+# A sad story.........
 ##########################################################################
 
-## Which state has largest number of bridges?
-
-# 1	State Code
-# 3	County (Parish) Code
-# 9	Location
+# library(leaflet)
 # 
-# grep("001|003|016|017", colnames(M))
-# colnames(M)[grep("001|003|016|017", colnames(M))]
-# placeCodeColName <- colnames(M)[grep("001|003|004", colnames(M))]
-
-# M %>% select(STATE_CODE_001, COUNTY_CODE_003, LAT_016, LONG_017) %>% mutate(code = STATE_CODE_001*1000+COUNTY_CODE_003) -> D1
-# 
-# # Fix the Place Code of the Single Digit State.
-
-# library(stringr)
-# D1$code <- str_pad(D1$code, 5, pad = "0")
-
-# D1%>% group_by(code) %>% dplyr::summarise(count = n())
-
-# 
-# blingIcon <- makeIcon(
-#   iconUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Emoji_u1f4b0.svg/1024px-Emoji_u1f4b0.svg.png",
-#   iconWidth = 30, iconHeight = 30,
-# )
+# ## filter data
+# d2<-filter(d2,
+#            is.na(code)==FALSE &       ## Key measurements aren't missing
+#              is.na(LAT)==FALSE &
+#              is.na(LONG)==FALSE &
+#              LAT>20 & LAT<50 &         ## Location is US 48
+#              LONG>(-130) & LONG<(-60))
 # 
 # 
-# 
-# map <- leaflet(schools) %>% 
+# m <- leaflet(d2) %>%
 #   setView(-93.65, 42.0285, zoom = 4) %>%
 #   addTiles() %>%
-#   addMarkers(~LAT_016, ~LONG_017, popup=~info,
+#   addMarkers(lat = ~LAT, lng = ~LONG,
 #              options = popupOptions(closeButton = TRUE),
-#              clusterOptions = markerClusterOptions(), 
-#              icon = blingIcon)
+#              clusterOptions = markerClusterOptions())
+# 
+# 
+# m
